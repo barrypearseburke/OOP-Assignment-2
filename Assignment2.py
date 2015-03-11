6862ba4f7b7080145d269082889e58a0718830a1
barrypearseburke <barry.burke3@mydit.ie>
2015-03-11 14:48:39 +0000

    Customers succesfully being created..

--- Assignment2.py
+++ Assignment2.py
@@ -14,24 +14,33 @@ import string
 #todo no:1 make 3 classes called building , a customer and elevator and init
 
 class Building:
-    def __init__(self,floors):
-        self.floors =floors
+    def __init__(self,floors,no_of_customers):
+        self.floors = floors
+        self.no_of_customers = no_of_customers
+        self.customers_waiting=[]
+        self.customers_served=[]
+        for person in range(0,self.no_of_customers):
+            aCustomer =Customer(self.floors)
+            self.customers_waiting.append(aCustomer)
 
-    def __repr__(self):
-        #this will print out building with elevator
-        pass
+    # def __repr__(self):
+    #     #this will print out building with elevator
+    #     pass
 
 
 class Customer:
     def __init__(self,floor):
         #todo set customer source and destination
-        Source = random.randint(0,floor)
-        destination =random.randint(0,floor)
+        #todo errorcheck so source !=des
+        self.Source = random.randint(0,floor)
+        self.destination =random.randint(0,floor)
+
 
 
 class Elevator:
     def __init__(self):
         self.location =0
+        self.customers_inside_elevators=[]
 
 
 
@@ -41,11 +50,10 @@ def main():
     No_of_floors =int(input("How many floors are in the building"))
     No_of_Customers =int(input("How many Customers are in the building"))
 
-    #todo make a for loop to makes a load of inistinats of the customers.
-    Cust =[]
-    for items in No_of_Customers:
-        ACustomer = Customer(No_of_floors)
-        cust.append(ACustomer)
+    x=Building(No_of_floors,No_of_Customers)
+
+
+
 
 
 if __name__ =="__main__":
