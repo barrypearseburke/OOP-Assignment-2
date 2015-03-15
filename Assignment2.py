b2d5f3a16c88a2139c12c59bb4de64ed3f3b2b89
barrypearseburke <barry.burke3@mydit.ie>
2015-03-15 19:15:17 +0000

    bug going into elevator , see todo note line 71

--- Assignment2.py
+++ Assignment2.py
@@ -10,7 +10,7 @@ import winsound
     Name of Assignment = Elevator Simulator
      """
 #################################################################################################
-#      PLEASE ENSURE THAT FILE "Elevator Music.wav" is in the same directory as this file.     #
+##     PLEASE ENSURE THAT FILE "Elevator Music.wav" is in the same directory as this file.     ##
 #################################################################################################
 
 #todo play elevator music when lift opens
@@ -30,7 +30,8 @@ class Building:
             aCustomer =Customer(self.floors)
             self.customers_waiting.append(aCustomer)
         #create an elevator
-            AElevator = Elevator(self.floors,self.customers_waiting,self.customers_served)
+        AElevator = Elevator(self.floors,self.customers_waiting,self.customers_served)
+        AElevator.Elevatormainloop()
     # def __repr__(self):
     #     #this will print out building with elevator..
     #     pass
@@ -38,6 +39,8 @@ class Building:
 class Elevator:
     def __init__(self,floors,customers_waiting,customers_served):
         self.location =0
+        self.direction = "up"
+        self.floors =floors
         self.customers_inside_elevators=[]
         self.customers_waiting =customers_waiting
         self.customers_served=customers_served
@@ -45,21 +48,67 @@ class Elevator:
 
     def Elevatormainloop(self):
         #check to see are their people in the list
+        self.working =0
+        while self.working ==0:
+            if len(self.customers_inside_elevators) == 0:#There is nobody in the lift
+                # pass for now. will go to check customers list
+                #move on to see are the customers waiting to get on at this floor.
+
+                #check end
+                self.checkend()
+
+                #if nobody in lift , call to check are their people wanting to get in
 
 
-        if len(self.customers_inside_elevators) == 0:#There is nobody in the lift
-            pass# pass for now. will go to check customers list
-            #move on to see are the customers waiting to get on at this floor.
-        elif len(self.customers_inside_elevators) > 0:
-            #call fxn to search through the list and see does anybody want to get out at this floor.
-            pass
 
+            elif len(self.customers_inside_elevators) > 0:
+                #call fxn to search through the list and see does anybody want to get out at this floor.
+                self.checkElevator()
+                #once everybody is asked do they want to get out
+                #ask does anybody want to get in
+            self.checkCustometers()
+
+    #todo ,BUG: when a customer is leaving the lift or entering lift what is happening is they are removed from where they were. if they were item
+    #todo, 3 for example , the list then searches element 4 but becuase an entry has been removed. the  orginal number 4 is now item 3
+    # todo and the list is now searching new element 4 and orginal numnber 5. so orginal number 4 is not being asked does he want to enter
     def checkElevator(self):
         for customer in self.customers_inside_elevators: #for every customer in the lift.
             if customer.destination == self.location: #ask them do they want to get out.
                 #move customer in customers served.
                 self.customers_served.append(customer)
                 self.customers_inside_elevators.remove(customer)
+
+    def checkCustometers(self):
+        for customer in self.customers_waiting:
+            if customer.Source == self.location: #if the customer is wants to get on at this floor,
+                self.customers_inside_elevators.append(customer) #place customer into elevator
+                self.customers_waiting.remove(customer) #The customer isent waiting anymore
+
+
+        #go up or down a floor
+        if self.direction == "up" and self.location<self.floors: #if we are going up and were not on the top floor
+            self.location+=1#go up
+
+        elif self.direction =="up" and self.location ==self.floors: #if we are going up but were on the top floor
+            self.direction ="down" # we decide to go back down
+            self.location +=-1  #we go down by 1 floor.
+
+        elif self.direction == "down" and self.location >0: # if we are going down and we are not on the ground floor
+            self.location +=-1 #go down
+        elif self.direction =="down" and self.location ==0: # if we are going down , but were on floor 0
+            self.direction = "up" # we go up
+            self.location +=1 #we  go up by 1 floor.
+
+
+
+
+    def checkend(self):
+        #todo make an end call.
+        #this fxn will check to see is the program over. This happens when there is nobody left in the elevator.
+
+        if len(self.customers_waiting) == 0:
+            #end program
+            self.working =1 #end elevator main loop
 
 
 
